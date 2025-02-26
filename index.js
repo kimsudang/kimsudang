e1f47db799667528e97f6a16ab7695394cc279f1
kimsudang <0ksuyeon0@gmail.com>
2025-02-26 13:33:04 +0900

    블로그 글 개수, 업데이트 시간 수정

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ const parser = new Parser({
   text += `<ul>`;
 
   // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
-  for (let i = 0; i < 8; i++) {
+  for (let i = 0; i < 10; i++) {
     const { title, link } = feed.items[i];
     console.log(`${i + 1}번째 게시물`);
     console.log(`추가될 제목: ${title}`);
